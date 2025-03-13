d4181c20d1c7bb467779ae35731e7eb25882195c
Anselem300 <Osamuel1@byupathway.edu>
2025-03-12 23:40:07 -0700

    Week02 Assignment completed

---
+++ week02/scripts/temples.js
@@ -0,0 +1,20 @@
+const hamButton = document.getElementById('menu');
+const navigation = document.getElementById('navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+
+    if (navigation.classList.contains('open')) {
+        hamButton.innerHTML = "❎";
+    } else {
+        hamButton.innerHTML = "☰";
+    }
+})
+
+const date = new Date();
+let currentYear = document.getElementById("currentyear");
+currentYear.textContent = date.getFullYear();
+
+let lastModif = new Date(document.lastModified);
+let lastModified = document.getElementById("lastModified");
+lastModified.textContent = `${lastModif.getMonth()}/${lastModif.getDay()}/${lastModif.getFullYear()}, ${lastModif.toLocaleTimeString()}`;
